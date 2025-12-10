b040f8540c1cf8a89e102e97f6d4693927541b00
pranavpawarr <pranavpawar503@gmail.com>
2025-12-11 00:04:09 +0530

    prod ready

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,27 +1,9 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const createPrismaClient = () => {
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  });
+export const prisma = globalThis.prisma || new PrismaClient();
 
-  const adapter = new PrismaPg(pool);
-
-  return new PrismaClient({
-    adapter,
-    log:
-      process.env.NODE_ENV === "development" ? ["query", "error"] : ["error"],
-  });
-};
-
-export const prisma = globalThis.prisma ?? createPrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-  globalThis.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
